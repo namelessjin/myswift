1bbe93bfadb037ca8f02ddfbf5d493c90a3b9976
None <jinsenwu@jinsens-macbook-pro.local>
2018-08-11 15:43:21 -0400

    First comment as gitter August 11 3:40pm 2018

--- myswift/ViewController.swift
+++ myswift/ViewController.swift
@@ -10,9 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
+    
+    var buttontouch = false
+    var buttonCount = 0
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        buttonCount += 1
+        print(buttonCount)
+        
+        if buttonCount >= 10{
+            if buttontouch == false{
+                view.backgroundColor = UIColor.red
+                buttontouch = true
+                myLabel.text = "I am the BEST!"
+            } else{
+                view.backgroundColor = UIColor.white
+                buttontouch = false
+                myLabel.text = "This Sucks!"
+                buttontouch = false
+            }
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+
     }
 
     override func didReceiveMemoryWarning() {
